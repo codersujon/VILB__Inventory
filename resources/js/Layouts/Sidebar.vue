55bb1f6d2460f61f5f9ba55a75800142c9c028cc
unknown <csesujon155@gmail.com>
2024-10-17 21:27:47 -0700

    layout making

---
+++ resources/js/Layouts/Sidebar.vue
@@ -0,0 +1,52 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div class="vertical-menu">
+        <div data-simplebar class="h-100">
+
+            <!-- User details -->
+            <div class="user-profile text-center mt-3">
+                <div class="">
+                    <img src="../../../public/backend/assets/images/users/avatar-1.jpg" alt=""
+                        class="avatar-md rounded-circle">
+                </div>
+                <div class="mt-3">
+                    <h4 class="font-size-16 mb-1">Julia Hudda</h4>
+                    <span class="text-muted"><i
+                            class="ri-record-circle-line align-middle font-size-14 text-success"></i>
+                        Online</span>
+                </div>
+            </div>
+
+            <!--- Sidemenu -->
+            <div id="sidebar-menu">
+                <!-- Left Menu Start -->
+                <ul class="metismenu list-unstyled" id="side-menu">
+                    <li class="menu-title">Menu</li>
+
+                    <li>
+                        <Link :href="route('dashboard')" class="waves-effect">
+                            <i class="ri-dashboard-line"></i>
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-mail-send-line"></i>
+                            <span>Email</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="email-inbox.html">Inbox</a></li>
+                            <li><a href="email-read.html">Read Email</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- Sidebar -->
+        </div>
+    </div>
+</template>
