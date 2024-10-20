4c092108d16590defb940d3f188800e158378804
unknown <csesujon155@gmail.com>
2024-10-19 17:34:18 -0700

    PreLoader Change

---
+++ resources/js/Components/PageHeader.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+<template>
+    <div class="page-header">
+        <div class="row align-items-end">
+            <div class="col-lg-8">
+                <div class="page-header-title">
+                    <div class="d-inline">
+                        <h4>Foo-Table</h4>
+                        <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="page-header-breadcrumb">
+                    <ul class="breadcrumb-title">
+                        <li class="breadcrumb-item">
+                            <a href="index-1.htm"> <i class="feather icon-home"></i> </a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Pages</a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Sample page</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,13 +1,13 @@
 <script setup>
+    import { Link } from '@inertiajs/vue3';
     import Topbar from './Topbar.vue';
-    import Sidebar from './Sidebar.vue';
 </script>
 
 <template>
     <div>
 
         <!-- Pre-loader start -->
-        <!-- <div class="theme-loader">
+        <div class="theme-loader">
             <div class="ball-scale">
                 <div class='contain'>
                     <div class="ring">
@@ -42,7 +42,7 @@
                     </div>
                 </div>
             </div>
-        </div> -->
+        </div>
         <!-- Pre-loader end -->
 
         <div id="pcoded" class="pcoded">
@@ -185,8 +185,53 @@
                 <div class="pcoded-main-container">
                     <div class="pcoded-wrapper">
 
-                        <Sidebar />
+                       <!-- sidebar -->
+                       <nav class="pcoded-navbar">
+                            <div class="pcoded-inner-navbar main-menu">
+                                <ul class="pcoded-item pcoded-left-item">
+
+                                    <li class="">
+                                        <Link :href="route('dashboard')">
+                                            <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                                            <span class="pcoded-mtext">Dashboard</span>
+                                        </Link>
+                                    </li>
+
+                                </ul>
+                                <ul class="pcoded-item pcoded-left-item">
+                                    <li class="pcoded-hasmenu">
+                                        <a href="javascript:void(0)">
+                                            <span class="pcoded-micon"><i class="feather icon-clipboard"></i></span>
+                                            <span class="pcoded-mtext">Form Components</span>
+                                        </a>
+                                        <ul class="pcoded-submenu">
+                                            <li class=" ">
+                                                <a href="form-elements-component.htm">
+                                                    <span class="pcoded-mtext">Form Components</span>
+                                                </a>
+                                            </li>
+                                            <li class=" ">
+                                                <a href="form-elements-add-on.htm">
+                                                    <span class="pcoded-mtext">Form-Elements-Add-On</span>
+                                                </a>
+                                            </li>
+                                            <li class=" ">
+                                                <a href="form-elements-advance.htm">
+                                                    <span class="pcoded-mtext">Form-Elements-Advance</span>
+                                                </a>
+                                            </li>
+                                            <li class=" ">
+                                                <a href="form-validation.htm">
+                                                    <span class="pcoded-mtext">Form Validation</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
                        
+                        <!-- page content -->
                         <div class="pcoded-content">
                             <div class="pcoded-inner-content">
                                 <div class="main-body">
